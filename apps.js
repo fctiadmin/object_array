f061a5c0f93fb1cc5a5dd1321ab6cf4bf344e295
Future Computer Training Institute <107338637+fctiadmin@users.noreply.github.com>
2023-05-23 09:08:43 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,18 @@
+// var student={sname:"Jamal Miah",height:"5.11",education:"CSE",hobby:"Cricket"};
+// document.getElementById('print').innerHTML=student.sname+"<br>"+student.height;
+// document.getElementById('print').style.color='red';
+// document.getElementById('print').style.fontSize='40px';
+
+
+// var sarray=["Jamal","Kamal","Rana","Rubel","Rasel"];
+// sarray.push("Aklima");
+// sarray.pop("Aklima");
+// document.write(sarray);
+
+
+function addStyle(){
+    document.getElementById('print').style.color='red';
+}
+function removeStyle(){
+    document.getElementById('print').style.color='';
+}
